fdff9a904d5da8f8ac176b720cedcab62f02a483
jonykrause <jokrause@team.mobile.de>
2018-04-05 17:25:46 +0200

    update

--- public/js/rides.js
+++ public/js/rides.js
@@ -5,6 +5,10 @@ const osmUrl='http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
 const osmAttrib='Map data © <a href="http://openstreetmap.org">OpenStreetMap</a> contributors';
 const osm = new L.TileLayer(osmUrl, { minZoom: 8, maxZoom: 18, attribution: osmAttrib });
 const marker = L.marker([Number(lat), Number(lon)]).addTo(map);
-marker.bindPopup(`Time: ${new Date(Number(time))}, Battery: ${battery}%`).openPopup();
-map.setView(new L.LatLng(Number(lat), Number(lon)), 16);
+const timeHtml = `<div>Time: ${new Date(Number(time))}</div>`;
+const batteryHtml = `<div>Battery: ${battery}%</div>`;
+const coordsHtml = `<div>Lat: ${lat}, Lon: ${lon}</div>`;
+
+marker.bindPopup(`${timeHtml}${coordsHtml}${batteryHtml}`).openPopup();
+map.setView(new L.LatLng(Number(lat), Number(lon)), 15);
 map.addLayer(osm);
