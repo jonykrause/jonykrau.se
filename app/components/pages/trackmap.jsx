d2004fcbf02e4e1a2f01429de5de1da30579c72b
jonykrause <jokrause@team.mobile.de>
2018-04-05 13:18:48 +0200

    update

--- app/components/pages/trackmap.jsx
+++ app/components/pages/trackmap.jsx
@@ -11,12 +11,23 @@ var spotwalla = '<iframe class="spotwalla" src="https://spotwalla.com/embed.php?
 
 var locus = '<iframe class="spotwalla" src="https://live-track.locusmap.eu/room/J5IK-SF5Q" width="100%" height="400" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe>';
 
+
 var TrackMap = React.createClass({
+  componentDidMount: function() {
+    console.log('hallooooooo');
+  },
   render: function() {
-    const { lat, lon, acc, time, battery } = this.props.tracking;
+    const { lat, lon, acc, time, battery } = this.props.tracking || {};
     console.log(lat, lon, acc, time, battery);
+    const leafletInit = 'var mymap = L.map("leafletMap").setView([51.505, -0.09], 13); console.log(mymap)';
     return (
       <Layout>
+       <link rel="stylesheet" href="https://unpkg.com/leaflet@1.3.1/dist/leaflet.css"
+         integrity="sha512-Rksm5RenBEKSKFjgI3a41vrjkw4EVPlJ3+OiI65vTjIdo9brlAacEuKOiQ5OFh7cOI1bkDwLqdLw3Zg0cRJAAQ=="
+         crossorigin=""/>
+       <script src="https://unpkg.com/leaflet@1.3.1/dist/leaflet.js"
+         integrity="sha512-/Nsx9X4HebavoBvEBuyp3I7od5tA0UzAxs+j83KgC8PU0kgB4XiK4Lfe4y4cgBtaRJQEIFCW+oC506aPT2L1zw=="
+         crossorigin=""></script>
         <div className="post">
             <h2>Follow the route</h2>
             <small>via Spotwalla</small>
@@ -25,6 +36,8 @@ var TrackMap = React.createClass({
             <small>via Locus</small>
             <div dangerouslySetInnerHTML={{ __html: locus }} />
             <h2>See latest position</h2>
+            <div id="leafletMap"></div>
+            <script dangerouslySetInnerHTML={{__html: leafletInit }}></script>
         </div>
       </Layout>
     );
